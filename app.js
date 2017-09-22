c785563dcf0236e64ad0b7e4a464ad4b61bbe97e
Akash Gajjar <akashgajjar8@gmail.com>
2017-09-22 20:07:29 +0530

    added CORS functionality for the API

--- app.js
+++ app.js
@@ -4,34 +4,46 @@ const bodyParser = require('body-parser');
 var app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
-app.use(function (req,res,next) {
+var words = [{
+    "word": "plyhistor",
+    "desc": "a person of great and varied learning."
+  },
+  {
+    "word": "oceanicity",
+    "desc": "the degree to which the climate of a place is influenced by the sea."
+  },
+  {
+    "word": "birthdom",
+    "desc": "the land of ones birth or ones inheritance."
+  }
+];
+
+app.use(function(req, res, next) {
   console.log(`${req.method} request for ${req.url} - ${JSON.stringify(req.body)}`);
   next();
 });
 
-var words  = [
-  {"word":"plyhistor","desc":"a person of great and varied learning."},
-  {"word":"oceanicity","desc":"the degree to which the climate of a place is influenced by the sea."},
-  {"word":"birthdom","desc":"the land of ones birth or ones inheritance."}
-];
-
 app.use(express.static("./public"));
 
-app.get('/request',function (req,res) {
+app.use(cors());
+
+app.get('/request', function(req, res) {
   res.json(words);
 });
 
-app.post('/request',function (req,res) {
+app.post('/request', function(req, res) {
   req.body.word = req.body.word.trim();
   req.body.desc = req.body.desc.trim();
   words.push(req.body);
   res.json(words);
 });
 
-app.delete('/request/:term',function (req,res) {
-  words = words.filter(function (definition) {
+app.delete('/request/:term', function(req, res) {
+  words = words.filter(function(definition) {
     return definition.word.toLowerCase() != req.params.term.toLowerCase();
   });
   res.json(words);
